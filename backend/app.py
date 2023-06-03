89f71c4108641ba101368e10b6aec9bdb8aa984e
Ian Day <ian@ianday.me>
2023-06-03 09:54:31 -0400

    add middleware

--- backend/app.py
+++ backend/app.py
@@ -3,14 +3,26 @@ from pathlib import Path
 from api.endpoints.calculate import router as calculate_router
 from fastapi import FastAPI, APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 
 
-
-
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
 
 # create app
 
 app = FastAPI(title="WhichPlates API", description="Calculate required plates and set weights",)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 root_router = APIRouter()
 @root_router.get("/", status_code=200, tags=['Home'])
